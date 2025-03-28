12bc22ebb872651f7eb03dd5711556dba1650419
Angiea18 <angie.a.zapata@gmail.com>
2025-03-27 21:38:18 -0500

    feat(user): add some  fields and complete Role enum

--- Backend/SPC.Data/Models/User.cs
+++ Backend/SPC.Data/Models/User.cs
@@ -4,15 +4,22 @@ namespace SPC.Data.Models;
 
 public class User : BaseEntity<int>
 {
-  public string Name{get; set;}
-  public string Surname{get; set;}
-  public string Email{get; set;}
-  public Role Role{get; set;}
+    public required string Name { get; set; }   
+    public required string Surname { get; set; }  
+    public required string Email { get; set; } 
+    public Role Role { get; set; }  // Tipo de usuario (Estudiante, Docente, etc.)  
+    public required string DocumentType { get; set; }  // CC, TI, CE, NIT, etc.  
+    public required string DocumentNumber { get; set; }    
+    public required string PasswordHash { get; set; }  // Contraseña (encriptada)  
+    public bool AceptoTerminos { get; set; }  // Checkbox de términos y condiciones  
+    public required string UserType { get; set; }
 
 }
 
 public enum Role
 {
-  User,
-  Admin
+    Estudiante,
+    Docente,
+    Fundacion_Administracion, 
+    Otro_Externo
 }
